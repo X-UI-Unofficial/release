afaeea159bf7454392cf63ff69fac16a8bd6bf64
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2022-11-20 16:34:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+cpl() {
+    if [[ x"${release}" == x"centos" ]]; then
+        sudo yum install dialog -y &> /dev/null
+    else
+        sudo apt-get install dialog -y &> /dev/null
+    fi
+}
+
+if ! dpkg-query -W -f='${Status}' dialog | grep "ok installed" &> /dev/null; then cpl; fi
+
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
@@ -8,7 +18,11 @@ plain='\033[0m'
 cur_dir=$(pwd)
 
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}Error: ${plain} must be root to run this script! \n" && exit 1
+if [ $EUID -ne 0 ]
+   then dialog --cursor-off-label --title "Error" --backtitle "X-UI Installer" --msgbox "Must be root to run this script !" 6 39
+   clear
+   exit
+fi
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
@@ -28,7 +42,9 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    echo -e "System version not detected by ${red}, please contact the script author! ${plain}\n" && exit 1
+    dialog --cursor-off-label --title "Error" --backtitle "X-UI Installer" --msgbox "System version not detected, please contact the script author !" 6 39
+    clear
+    exit
 fi
 
 arch=$(arch)
@@ -43,14 +59,17 @@ elif [[ $arch == "riscv64" ]]; then
     arch="riscv64"
 else
     arch="x86_64"
-    echo -e "${red}Failed to detect schema, use default schema: ${arch}${plain}"
+    dialog --cursor-off-label --title "Warning" --backtitle "X-UI Installer" --msgbox "Failed to detect schema, use default schema: ${arch}" 6 39
+    clear
 fi
 
-echo "Your CPU arch: ${arch}"
+dialog --cursor-off-label --title "Infomation" --backtitle "X-UI Installer" --msgbox "Your CPU arch: ${arch}" 6 39
+clear
 
 if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ]; then
-    echo "This software does not support 32-bit system (x86), please use 64-bit system (x86_64), if the detection is wrong, please contact the author"
-    exit -1
+    dialog --cursor-off-label --title "Error" --backtitle "X-UI Installer" --msgbox "This software does not support 32-bit system (x86), please use 64-bit system (x86_64), if the detection is wrong, please contact the author" 6 39
+    clear
+    exit
 fi
 
 os_version=""
@@ -65,41 +84,116 @@ fi
 
 if [[ x"${release}" == x"centos" ]]; then
     if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}Please use CentOS 7 or higher! ${plain}\n" && exit 1
+        dialog --cursor-off-label --title "Error" --backtitle "X-UI Installer" --msgbox "Please use CentOS 7 or higher!" 6 39
+        clear
+        exit
     fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
     if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}Please use Ubuntu 16 or later! ${plain}\n" && exit 1
+        dialog --cursor-off-label --title "Error" --backtitle "X-UI Installer" --msgbox "Please use Ubuntu 16 or later!" 6 39
+        clear
+        exit
     fi
 elif [[ x"${release}" == x"debian" ]]; then
     if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}Please use Debian 8 or higher!${plain}\n" && exit 1
+        dialog --cursor-off-label --title "Error" --backtitle "X-UI Installer" --msgbox "Please use Debian 8 or higher!" 6 39
+        clear
+        exit
     fi
 fi
 
+# install_base() {
+#     if [[ x"${release}" == x"centos" ]]; then
+#         yum install wget curl tar newt -y
+#     else
+#         apt-get install wget curl tar whiptail -y
+#     fi
+# }
+
 install_base() {
-    if [[ x"${release}" == x"centos" ]]; then
-        yum install wget curl tar newt -y
-    else
-        apt-get install wget curl tar whiptail -y
-    fi
+msgs=("Preparing install..."
+      "Starting wget installation..."
+      "Starting curl installation..."
+      "Starting tar installation..."
+     )
+if [[ x"${release}" == x"centos" ]]; then
+    commands=("yum install wget -y"
+              "yum install curl -y"
+              "yum install tar -y"
+              )
+else
+    commands=("apt-get install wget -y"
+              "apt-get install curl -y"
+              "apt-get install tar -y"
+              )
+fi
+n=${#commands[@]}
+i=0
+while [ "$i" -lt "$n" ]; do
+    pct=$(( i * 100 / n ))
+    echo XXX
+    echo $i
+    echo "${msgs[i]}"
+    echo XXX
+    echo "$pct"
+    eval "${commands[i]}"
+    i=$((i + 1))
+done | dialog --cursor-off-label --title "X-UI Installer Packages" --backtitle "X-UI Installer" --gauge "Please wait install ..." 10 60 0
+clear
 }
 
 config_after_install() {
-    whiptail --title "Warning" --msgbox "For security reasons, it is necessary to forcibly modify the port and account password after the installation/update is completed." 8 78
-    if (whiptail --title "Confirm whether to continue ?" --yesno "Select Yes if you want to setup username / password X-UI, select No for default Login." 8 78); then
-        config_account=$(whiptail --inputbox "Please set your account name" 8 39 --title "Create Account" 3>&1 1>&2 2>&3)
+    config_account=""
+    config_password=""
+    config_port=""
+    dialog --cursor-off-label \
+           --title "Warning" \
+           --backtitle "X-UI Installer" \
+           --yesno "Are you sure you want Setting account install X-UI ?" 7 60
+    response=$?
+    case $response in
+       0)
+        config_account=""
+        config_password=""
+        config_port=""
+        dialog --cursor-off-label \
+               --title "Warning" \
+               --backtitle "X-UI Installer" --ok-label "OK" \
+               --stdout \
+               --form "Settings X-UI Config" 10 60 3 \
+                      "username: " 1 1 "$config_account" 1 15 30 0 \
+                      "password: " 2 1 "$config_password" 2 15 30 0 \
+                      "port: " 3 1 "$config_port" 3 15 30 0 > /tmp/xuioutput.txt
+        config_account=$(cat /tmp/xuioutput.txt | head -1)
+        config_password=$(cat /tmp/xuioutput.txt | head -2 | tail -1)
+        config_port=$(cat /tmp/xuioutput.txt | head -3 | tail -1)
+        rm -rf /tmp/xuioutput.txt
+
+        # config_account=$(whiptail --inputbox "Please set your account name" 8 39 --title "Create Account" 3>&1 1>&2 2>&3)
         [[ ! -z "${config_account}" ]] || config_account="admin"
-        config_password=$(whiptail --passwordbox "Please set your password" 8 39 --title "Create Account" 3>&1 1>&2 2>&3)
+        # config_password=$(whiptail --passwordbox "Please set your password" 8 39 --title "Create Account" 3>&1 1>&2 2>&3)
         [[ ! -z "${config_password}" ]] || config_password="admin"
-        config_port=$(whiptail --inputbox "Please set your port" 8 39 --title "Setup X-UI" 3>&1 1>&2 2>&3)
+        # config_port=$(whiptail --inputbox "Please set your port" 8 39 --title "Setup X-UI" 3>&1 1>&2 2>&3)
         [[ ! -z "${config_port}" ]] || config_port="54321"
         /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
         /usr/local/x-ui/x-ui setting -port ${config_port}
-        whiptail --title "X-UI Installer" --msgbox "X-UI Settings Complete" 8 78
-    else
-        whiptail --title "X-UI Installer" --msgbox "Cancelled, all setting items are default settings, please modify in time.\nDefault username: admin \nDefault password: admin \nDefault port: 54321" 12 80
-    fi
+        clear
+        dialog --cursor-off-label \
+               --title "Complete" \
+               --backtitle "X-UI Installer" \
+               --msgbox "X-UI Settings Complete" 8 78
+        clear
+        ;;
+       1)
+        dialog --cursor-off-label \
+               --title "Complete" \
+               --backtitle "X-UI Installer" \
+               --msgbox "Cancelled, all setting items are default settings, please modify in time.\nDefault username: admin \nDefault password: admin \nDefault port: 54321" 12 80
+        clear
+        ;;
+       255) echo "[ESC] key pressed."
+        ;;
+    esac
 }
 
 install_x-ui() {
@@ -109,21 +203,24 @@ install_x-ui() {
     if [ $# == 0 ]; then
         last_version=$(curl -Ls "https://api.github.com/repos/X-UI-Unofficial/release/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
-            whiptail --title "X-UI Installer" --msgbox "The failure of the X-UI version may be beyond the GitHub API limit, please try it later, or manually specify the X-UI version installation" 8 78
-            exit 1
+            dialog --cursor-off-label --title "Error" --backtitle "X-UI Installer" --msgbox "The failure of the X-UI version may be beyond the GitHub API limit, please try it later, or manually specify the X-UI version installation." 6 39
+            clear
+            exit
         fi
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/X-UI-Unofficial/release/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz 2>&1 |  stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' |  whiptail --title "X-UI Installer" --gauge "Download X-UI ..." 6 50 0
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/X-UI-Unofficial/release/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz 2>&1 |  stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | dialog --cursor-off-label --title "Download..." --backtitle "X-UI Installer" --gauge "Download X-UI ..." 6 50 0
         if [[ $? -ne 0 ]]; then
-            whiptail --title "X-UI Installer" --msgbox "Failed to download x-ui, please make sure your server can download Github files" 8 78
-            exit 1
+            dialog --cursor-off-label --title "Error" --backtitle "X-UI Installer" --msgbox "Failed to download x-ui, please make sure your server can download Github files." 6 39
+            clear
+            exit
         fi
     else
         last_version=$1
         url="https://github.com/X-UI-Unofficial/release/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url} 2>&1 |  stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' |  whiptail --title "X-UI Installer" --gauge "Download X-UI ..." 6 50 0
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url} 2>&1 |  stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | dialog --cursor-off-label --title "Download..." --backtitle "X-UI Installer" --gauge "Download X-UI ..." 6 50 0
         if [[ $? -ne 0 ]]; then
-            whiptail --title "X-UI Installer" --msgbox "Failed to download x-ui v$1, please make sure this version exists" 8 78
-            exit 1
+            dialog --cursor-off-label --title "Complete" --backtitle "X-UI Installer" --msgbox "Failed to download x-ui v$1, please make sure this version exists." 6 39
+            clear
+            exit
         fi
     fi
 
@@ -152,8 +249,9 @@ install_x-ui() {
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-    whiptail --title "X-UI Installer" --msgbox "Install X-UI Complete: ${last_version} \nUse command x-ui for more infomation." 8 78
+    dialog --cursor-off-label --title "Complete" --backtitle "X-UI Installer" --msgbox "Install X-UI Complete: ${last_version} \nUse command x-ui for more infomation." 8 45
 }
-whiptail --title "X-UI Installer" --msgbox "Start install X-UI" 8 78
+dialog --cursor-off-label --title "Complete" --backtitle "X-UI Installer" --msgbox "Start install X-UI" 8 45
 install_base
 install_x-ui $1
+clear
